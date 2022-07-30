c583a71670e5e93095c643d66016488088901515
nawawa <oonawa12@gmail.com>
2022-07-30 16:26:55 +0900

    ログインページ追加

---
+++ src/pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h1>ログイン</h1>
+    <form>
+      <div>
+        <label for="email">email</label>
+        <input type="text" id="email" v-model="email" />
+      </div>
+      <div>
+        <label for="password">password</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  auth: false,
+  data: () => ({
+    email: '',
+    password: '',
+  })
+}
+</script>
